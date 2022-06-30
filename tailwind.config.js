66e68b4ffe6ff7a7a3c089467a987eb91d38a3ed
mkfizi <mkfizi93@gmail.com>
2022-06-30 23:33:15 +0800

    Update viewport height fix fallback value

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     mode: 'jit',
@@ -10,10 +10,10 @@ module.exports = {
     theme: {
         extend: {
             minHeight: {
-                "screen": [defaultTheme.minHeight.screen, "calc(var(--vh, 1vh) * 100)"],
+                "screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
             },
             height: {
-                "screen": [defaultTheme.height.screen, "calc(var(--vh, 1vh) * 100)"],
+                "screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
             }
         }
     },
