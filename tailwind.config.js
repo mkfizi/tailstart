4844f2505491a9e2bff7365f6ca0ce4bf54891f0
Khafizi Noh <khafizi.noh@gmail.com>
2023-06-19 16:17:03 +0800

    Update node package versions, change npm run script, rewrite tailwind config notes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,21 @@ module.exports = {
         "./*.html",
         "./src/**/*.{html,js}"
     ],
-    theme: {
+    theme: {	
         extend: {
             /**
-             * Below are fixes to handle viewport height issue on mobile browsers
+             * This is workaround fix to handle viewport height issue on mobile browsers
              * https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser
+             * 
+             * Below are fallback value.
              */
             minHeight: {
                 "screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
             },
             height: {
-                "screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
-            }
-        }
+				"screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
+            },
+        },
     },
     plugins: [],
 }
