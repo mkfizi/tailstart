8bebd306bd588278b453355b6002e71b780770e7
Khafizi Noh <khafizi.noh@gmail.com>
2023-06-19 16:16:25 +0800

    Rewrite `app.js` scripts

--- src/js/app.js
+++ src/js/app.js
@@ -1,96 +1,110 @@
 /**
  * --------------------------------------------------------------------------
- * Tailstart (v0.3.2): app.js
+ * Tailstart v1.0.0: app.js
  * Licensed under MIT (https://github.com/mkfizi/tailstart/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
 
-"use strict";
+'use strict';
 
 let app = {
-    version: "0.3.2",
+    name: 'Tailstart',
+};
 
-    // Set the events for resizing and scrolling the window, and for toggling dark mode.
-    setEvents(){
-        window.addEventListener("resize", this.updateViewportHeight);
-        window.addEventListener("scroll", this.updateNavbar);
+app.elements = {
+    darkModeToggle: document.getElementById('darkModeToggle'),
+    footerCurrentYear: document.getElementById('footerCurrentYear'),
+    footerAppName: document.getElementById('footerAppName'),
+};
 
-        const darkModeToggle = document.getElementById("darkModeToggle");
-        if (darkModeToggle) darkModeToggle.addEventListener("click", this.updateDarkMode.bind(this));
+app.config = {
+    isMenuActive: false
+}
+
+app.init = () => {
+    app.view.init();
+    app.event.init();
+};
+
+app.event = {
+    init: () => {
+        document.addEventListener('click', app.event.handleClick);
+        window.addEventListener('resize', app.view.updateViewportHeight);
     },
 
-    // Update the height of the viewport.
-    updateViewportHeight() {
-        document.documentElement.style.setProperty("--vh", (window.innerHeight * 0.01) + "px");
-    },
+    handleClick: event => {
+        const target = event.target;
 
-    // Update the navbar's appearance based on window scroll position.
-    updateNavbar() {
-        const navbar = document.getElementById("navbar");
-
-        if (navbar) {
-            // Check if window scroll position is below navbar position.
-            window.pageYOffset > (navbar.offsetHeight - navbar.clientHeight)
-                ? navbar.classList.add('bg-white', 'dark:bg-neutral-800', 'shadow') 
-                : navbar.classList.remove('bg-white', 'dark:bg-neutral-800', 'shadow');
+        if (target.closest('[id="darkModeToggle"]')) {
+            app.view.updateDarkMode();
         }
-    },
-
-    // Toggle between light and dark mode.
-    updateDarkMode() {
-        app.util.addRemoveTransition();
-        if (localStorage.theme === "light" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: light)").matches)) {
-            localStorage.theme = 'dark';
-            document.documentElement.classList.add("dark");
-        } else if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-            localStorage.theme = 'light';
-            document.documentElement.classList.remove("dark");
-        };
-    },
-
-    // Update app current version
-    updateAppCurrentVersion() {
-        const headerCurrentVersion = document.getElementById("headerCurrentVersion");
-        const footerCurrentVersion = document.getElementById("footerCurrentVersion");
-        if (headerCurrentVersion) headerCurrentVersion.innerHTML = this.version;
-        if (footerCurrentVersion) footerCurrentVersion.innerHTML = this.version;
-    },
-
-    // Update footer current year.
-    updateFooterCurrentYear() {
-        const footerCurrentYear = document.getElementById("footerCurrentYear");
-        if (footerCurrentYear) footerCurrentYear.innerHTML = new Date().getFullYear();
-    },
-
-    // Initialize app.
-    init() {
-        this.setEvents();
-        this.updateViewportHeight();
-        this.updateNavbar();
-        this.updateAppCurrentVersion();
-        this.updateFooterCurrentYear();
     },
 };
 
-// Helper functions for the app.
-app.util = {
-    /**
-     * Add and then remove a transition effect to prevent FOUC.
-     * 
-     * Note:
-     * For this to work, make sure 'transition-none' is defined after
-     * 'transition' and 'transition-*' classes in CSS output file.
-     * Initialize app.
-     */
-    addRemoveTransition() {
-        const transitions = document.querySelectorAll(".transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform");
-        for (const transition of transitions) {
-            transition.classList.add("transition-none");
-            setTimeout(function(){ 
-                transition.classList.remove("transition-none"); 
-            }, 50);
-        };
+app.view = {
+    init: () => {
+        app.view.updateViewportHeight();
+        app.view.updateAppInfo();
+    },
+
+    // Update the height of the viewport. This is a workaround fix for [viewport height issue on mobile browsers](https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser) 
+    updateViewportHeight: () => {
+        document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
+    },
+
+    // Update navbar appearance based on window scroll position
+    updateNavbar: () => {
+        if (!app.elements.navbar && !app.elements.navbarContent) return;
+
+        const isNavbarScrolled = window.pageYOffset > app.elements.navbar.offsetHeight;
+
+        app.elements.navbar.classList.toggle('lg:h-32', !isNavbarScrolled);
+        app.elements.navbar.classList.toggle('lg:border-transparent', !isNavbarScrolled);
+        app.elements.navbar.classList.toggle('lg:dark:border-transparent', !isNavbarScrolled);
+        app.elements.navbar.classList.toggle('lg:border-neutral-100', isNavbarScrolled);
+        app.elements.navbar.classList.toggle('lg:dark:border-neutral-800', isNavbarScrolled);
+        app.elements.navbarImage.classList.toggle('lg:w-24', !isNavbarScrolled);
+        app.elements.navbarImage.classList.toggle('lg:h-24', !isNavbarScrolled);
+        app.elements.navbarImage.classList.toggle('lg:-left-28', !isNavbarScrolled);
+        app.elements.navbarImage.classList.toggle('lg:-left-20', isNavbarScrolled);
+    },
+
+    // Update dark mode based on value in 'localStorage.theme'
+    updateDarkMode: () => {
+        app.util.toggleTransition();
+
+        if (localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches)) {
+            localStorage.theme = 'dark';
+            document.documentElement.classList.add('dark');
+        } else if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            localStorage.theme = 'light';
+            document.documentElement.classList.remove('dark');
+        }
+    },
+
+    // Update the footer with current year, app name, and version
+    updateAppInfo: () => {
+        if (app.elements.footerCurrentYear) {
+            app.elements.footerCurrentYear.innerHTML = new Date().getFullYear();
+        }
+
+        if (app.elements.footerAppName) {
+            app.elements.footerAppName.innerHTML = app.name;
+        }
     }
-}
+};
+
+app.util = {
+    // Toggle CSS transitions for smoother element transitions
+    toggleTransition: () => {
+        const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
+        for (const transition of transitions) {
+            transition.classList.add('transition-none');
+            setTimeout(() => {
+                transition.classList.remove('transition-none');
+            }, 100);
+        }
+    }
+};
 
 app.init();
