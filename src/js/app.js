69c932f0e7bfb86862b90d905647ae307b46f557
Khafizi Noh <khafizi.noh@gmail.com>
2023-06-20 13:24:35 +0800

    Add more event handler, view, and utility functions in `app.js`

--- src/js/app.js
+++ src/js/app.js
@@ -12,13 +12,17 @@ let app = {
 };
 
 app.elements = {
+    navbar: document.getElementById('navbar'),
+    navbarMenu: document.getElementById('navbarMenu'),
     darkModeToggle: document.getElementById('darkModeToggle'),
+    navbarMenuToggle: document.getElementById('navbarMenuToggle'),
     footerCurrentYear: document.getElementById('footerCurrentYear'),
     footerAppName: document.getElementById('footerAppName'),
 };
 
 app.config = {
-    isMenuActive: false
+    isMenuActive: false,
+    responsiveBreakpoint: 640
 }
 
 app.init = () => {
@@ -28,17 +32,29 @@ app.init = () => {
 
 app.event = {
     init: () => {
-        document.addEventListener('click', app.event.handleClick);
-        window.addEventListener('resize', app.view.updateViewportHeight);
+        document.addEventListener('click', app.event.handleDocumentClick);
+        window.addEventListener('resize', app.event.handleWindowResize);
+        window.addEventListener('scroll', app.event.handleWindowScroll);
     },
 
-    handleClick: event => {
+    handleDocumentClick: event => {
         const target = event.target;
 
         if (target.closest('[id="darkModeToggle"]')) {
             app.view.updateDarkMode();
+        } else if (target.closest('[id="navbarMenuToggle"]')) {
+            app.view.toggleNavbarMenu();
         }
     },
+
+    handleWindowResize: () => {
+        app.view.updateViewportHeight();
+        app.view.updateNavbarMenu();
+    },
+
+    handleWindowScroll: () => {
+        app.view.updateNavbar();
+    }
 };
 
 app.view = {
@@ -54,19 +70,14 @@ app.view = {
 
     // Update navbar appearance based on window scroll position
     updateNavbar: () => {
-        if (!app.elements.navbar && !app.elements.navbarContent) return;
+        if (!app.elements.navbar) return;
 
-        const isNavbarScrolled = window.pageYOffset > app.elements.navbar.offsetHeight;
+        const isNavbarScrolled = window.pageYOffset > (app.elements.navbar.offsetHeight - app.elements.navbar.clientHeight);
 
-        app.elements.navbar.classList.toggle('lg:h-32', !isNavbarScrolled);
-        app.elements.navbar.classList.toggle('lg:border-transparent', !isNavbarScrolled);
-        app.elements.navbar.classList.toggle('lg:dark:border-transparent', !isNavbarScrolled);
-        app.elements.navbar.classList.toggle('lg:border-neutral-100', isNavbarScrolled);
-        app.elements.navbar.classList.toggle('lg:dark:border-neutral-800', isNavbarScrolled);
-        app.elements.navbarImage.classList.toggle('lg:w-24', !isNavbarScrolled);
-        app.elements.navbarImage.classList.toggle('lg:h-24', !isNavbarScrolled);
-        app.elements.navbarImage.classList.toggle('lg:-left-28', !isNavbarScrolled);
-        app.elements.navbarImage.classList.toggle('lg:-left-20', isNavbarScrolled);
+        app.elements.navbar.classList.toggle('border-neutral-200', isNavbarScrolled);
+        app.elements.navbar.classList.toggle('dark:border-neutral-800', isNavbarScrolled);
+        app.elements.navbar.classList.toggle('border-transparent', !isNavbarScrolled);
+        app.elements.navbar.classList.toggle('dark:border-transparent', !isNavbarScrolled);
     },
 
     // Update dark mode based on value in 'localStorage.theme'
@@ -91,10 +102,101 @@ app.view = {
         if (app.elements.footerAppName) {
             app.elements.footerAppName.innerHTML = app.name;
         }
+    },
+
+    // Toggle the navbar menu visibility and update related elements
+    toggleNavbarMenu: () => {
+        app.util.toggleTransitionAll(app.elements.navbarMenu);
+
+        app.elements.navbarMenu.classList.toggle('hidden', app.config.isNavbarMenuActive);
+        app.elements.navbarMenu.classList.toggle('flex', !app.config.isNavbarMenuActive);
+        app.util.focusable[app.config.isNavbarMenuActive ? 'disable' : 'enable'](app.elements.navbarMenu);
+        app.util.static[app.config.isNavbarMenuActive ? 'disable' : 'enable']();
+        app.util.focusTrap[app.config.isNavbarMenuActive ? 'disable' : 'enable'](app.elements.navbarMenu);
+      
+        app.elements.navbarMenuToggle.setAttribute('aria-expanded', !app.config.isNavbarMenuActive);
+        app.config.isNavbarMenuActive = !app.config.isNavbarMenuActive;
+    },
+
+    updateNavbarMenu: () => {
+        if (window.innerWidth >= app.config.responsiveBreakpoint && app.config.isNavbarMenuActive) {
+            app.view.toggleNavbarMenu();
+        }
     }
 };
 
 app.util = {
+    static: {
+        enable: () => {
+            document.getElementsByTagName("BODY")[0].classList.add('overflow-hidden');
+        },
+
+        disable: () => {
+            document.getElementsByTagName("BODY")[0].classList.remove('overflow-hidden');
+        }
+    },
+
+    focusable: {
+        selector: `a, button, input, textarea, select, details, [contenteditable="true"]`,
+
+        // Enable focus on the specified element and its focusable child elements
+        enable: element => {
+            for (const focusableElement of element.querySelectorAll(app.util.focusable.selector)) {
+                focusableElement.classList.remove("invisible");
+            }
+        },
+
+        // Disable focus on the specified element and its focusable child elements
+        disable: element => {
+            for (const focusableElement of element.querySelectorAll(app.util.focusable.selector)) {
+                focusableElement.classList.add("invisible");
+            }
+        },
+    },
+
+    focusTrap: {
+        active: {},
+        selector: `a:not([tabindex="-1"]), button:not([tabindex="-1"]), input:not([tabindex="-1"]), textarea:not([tabindex="-1"]), select:not([tabindex="-1"]), details:not([tabindex="-1"]), [tabindex]:not([tabindex="-1"]), [contenteditable="true"]:not([tabindex="-1"])`,
+
+        handleEvent: (event, element) => {
+            const focusableElements = element.querySelectorAll(app.util.focusTrap.selector);
+            const firstElement = focusableElements[0];
+            const lastElement = focusableElements[focusableElements.length - 1];
+        
+            if (event.type === "keydown" && event.keyCode === 9) {
+                if (event.shiftKey && (document.activeElement === firstElement || document.activeElement === document.body)) {
+                    event.preventDefault();
+                    lastElement.focus();
+
+                } else if (!event.shiftKey && document.activeElement === lastElement) {
+                    event.preventDefault();
+                    firstElement.focus();
+                }
+            }
+        },
+
+        enable: element => {
+            const id = element.getAttribute("id");
+            app.util.focusTrap.active[id] = event => app.util.focusTrap.handleEvent(event, element)
+            window.addEventListener("keydown", app.util.focusTrap.active[id]);
+            setTimeout(function() { 
+                element.setAttribute("tabindex", 0);
+                element.focus();
+            }, 50);
+
+            setTimeout(function() { 
+                element.removeAttribute("tabindex"); 
+                element.blur();
+            }, 100);
+        },
+
+        disable: element => {
+            const id = element.getAttribute("id");
+            window.removeEventListener("keydown", app.util.focusTrap.active[id]);
+            app.util.focusTrap.active[id] = null;
+        }
+    },
+
     // Toggle CSS transitions for smoother element transitions
     toggleTransition: () => {
         const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
@@ -102,8 +204,16 @@ app.util = {
             transition.classList.add('transition-none');
             setTimeout(() => {
                 transition.classList.remove('transition-none');
-            }, 100);
+            }, 150);
         }
+    },
+
+    // Toggle CSS transition on the specified element
+    toggleTransitionAll: element => {
+        element.classList.add('transition-all');
+        setTimeout(() => {
+            element.classList.remove('transition-all'); 
+        }, 150);
     }
 };
 
